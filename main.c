16c014b25a94cebc1bb9bf4154a45acc83c8368a
mrRamezanzad <8212163+mrRamezanzad@users.noreply.github.com>
2020-04-25 08:41:51 +0000

    Improved timer

--- main.c
+++ main.c
@@ -2,26 +2,30 @@
 #include <time.h>
 #include <stdbool.h>
 
+
 int main (){
 
   time_t start, end;
-  double dif;
-  double duration = 4;	//duration of timer
-  bool loop = true;
-  while (loop == true)
-    {
-      time (&start);
-       printf("hello");
-      if (dif == duration)
-      
-	{
-	  /*callback */
-	  dif = 0;
-	}
-      //do stuff
-     
-      time (&end);
-      dif += difftime (end, start);
-    }
+  int dif;
+
+  time (&start);
+  
+  int c, d;
+      for (c = 1; c <= 32767; c++)
+       for (d = 1; d <= 2; d++)
+       {printf("hello\n");}
+  
+  time (&end);
+  
+  dif += difftime (end, start); 
+  printf("%d\t", dif);
+
+  if(dif >= 5){
+    printf("I will fully open the sunroof");
+  }
+
+  if(dif <= 5){
+    printf("I will mid_open the sunroof");
+  }
   return 0;
 }
