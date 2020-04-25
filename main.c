cc2a7b7a158100b47d80de3956e5d2dfa60af390
mrRamezanzad <8212163+mrRamezanzad@users.noreply.github.com>
2020-04-25 10:37:56 +0000

    todo list added

--- main.c
+++ main.c
@@ -3,15 +3,29 @@
 #include <stdbool.h>
 
 
+
 int main (){
 
+/*TODO: 
+1) initialize the input output pins
+2) set the signals of port registers
+3) check which button is pushed close or open
+4) set timer to check how long the button has been pushed 500ms and 1000ms
+5) if the close button pushed cancel the open
+6) if the reset is pushed cancel the signal (girpage)
+
+
+
+
+fsdaf
+*/
   time_t start, end;
   int dif;
 
   time (&start);
   
   int c, d;
-      for (c = 1; c <= 32767; c++)
+    for (c = 1; c <= 32767; c++)
        for (d = 1; d <= 2; d++)
        {printf("hello\n");}
   
